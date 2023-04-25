bc0b1b46399e950f1bb99b1adc488760c959c3c2
Jddelara01 <jddelara01@gmail.com>
2023-04-25 13:22:50 +0000

    Add htmls to render view

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Booking
 
@@ -6,4 +6,4 @@ from .models import Booking
 class BookingDetail(View):
 
     def get(self, request):
-        return render(request, 'booking.html')
+        return render(request, 'index.html')
