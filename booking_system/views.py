f0a5b64c1580cdf97c4b1634483a16cfb5cf3a79
Jddelara01 <jddelara01@gmail.com>
2023-04-28 12:16:00 +0000

    Add booking form

--- booking_system/views.py
+++ booking_system/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Booking
 from django.http import HttpResponse
+from .forms import BookingForm
 
 
 class BookingDetail(View):
@@ -11,5 +12,18 @@ class BookingDetail(View):
         booking = get_object_or_404(queryset)
 
         return render(request, 'booking.html', {
-            "booking": booking
+            "booking": booking,
+            "booking_form": BookingForm()
+        },)
+
+    def post(self, request, *args, **kw):
+        queryset = Booking.objects
+        booking = get_object_or_404(queryset)
+
+        booking_form = BookingForm(data=request.POST)
+        booking_form.save()
+
+        return render(request, 'booking.html', {
+            "booking": booking,
+            "booking_form": BookingForm()
         },)
