65d5f05ce9c31b80183eecdddfcddbfd727ccd83
Jddelara01 <jddelara01@gmail.com>
2023-05-02 14:41:11 +0000

    Autoselect table number per booking

--- booking_system/views.py
+++ booking_system/views.py
@@ -9,8 +9,8 @@ import warnings
 class BookingDetail(View):
 
     def get(self, request, *args, **kw):
-        queryset = Booking.objects
-        booking = get_object_or_404(queryset)
+        booking_query = Booking.objects.filter(email='jane@test.com')
+        booking = get_object_or_404(booking_query)
 
         return render(request, 'booking.html', {
             "booking": booking,
@@ -18,16 +18,22 @@ class BookingDetail(View):
         },)
 
     def post(self, request, *args, **kw):
-        queryset = Booking.objects
-        queryset1 = Table.objects.filter(table_num=1)
-        booking = get_object_or_404(queryset)
-        table = get_object_or_404(queryset1)
+        booking_query = Booking.objects.filter(email='jane@test.com')
+        booking = get_object_or_404(booking_query)
 
         booking_form = BookingForm(data=request.POST)
         if booking_form.is_valid():
-            # booking_form.save(commit=False)
-            booking_form.instance.table_num = table
+            booking_form.save(commit=False)
+            if booking_form.cleaned_data['num_of_people'] == 4:
+                table_query = Table.objects.filter(table_num=1)
+                table = get_object_or_404(table_query)
+                booking_form.instance.table_num = table
+            else:
+                table_query1 = Table.objects.filter(table_num=2)
+                table1 = get_object_or_404(table_query1)
+                booking_form.instance.table_num = table1
             booking_form.save()
+
             return render(request, 'booking.html', {
                 "booking": booking,
                 "booking_form": BookingForm()
