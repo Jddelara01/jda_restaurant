8ed762cb0e8f614a488ec52668a756d0be0246b2
Jddelara01 <jddelara01@gmail.com>
2023-05-09 08:27:05 +0000

    Update db and change to function base views

--- booking_system/models.py
+++ booking_system/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Table Model
 class Table(models.Model):
-    table_num = models.IntegerField(null=False, unique=True)
-    max_capacity = models.IntegerField(null=False)
+    """
+    These will be the tables that can be assigned to a booking that will
+    depend on the capacity of the table and the number of people per booking
+    """
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.size)
 
 
 # Booking Model
@@ -17,8 +24,13 @@ class Booking(models.Model):
     phone = PhoneNumberField(unique=True, region='IE')
     date = models.DateField()
     time = models.TimeField()
-    num_of_people = models.IntegerField(null=False)
-    table_num = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='bookings')  # noqa
+    num_of_people = models.IntegerField(
+        validators=[
+            MaxValueValidator(8),
+            MinValueValidator(1)
+        ]
+    )
+    table_num = models.ManyToManyField(Table)
 
     def __str__(self):
         return self.fullname
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import generic, View
 from .models import Booking, Table
 from django.http import HttpResponse
@@ -6,37 +6,38 @@ from .forms import BookingForm
 import warnings
 
 
-class BookingDetail(View):
+def say_hello(request):
+    return HttpResponse('Hello!')
 
-    def get(self, request, *args, **kw):
-        booking_query = Booking.objects.filter(email='jane@test.com')
-        booking = get_object_or_404(booking_query)
+    # def get(self, request, *args, **kw):
+    #     booking_query = Booking.objects.filter(email='jane@test.com')
+    #     booking = get_object_or_404(booking_query)
 
-        return render(request, 'booking.html', {
-            "booking": booking,
-            "booking_form": BookingForm()
-        },)
+    #     return render(request, 'booking.html', {
+    #         "booking": booking,
+    #         "booking_form": BookingForm()
+    #     },)
 
-    def post(self, request, *args, **kw):
-        booking_query = Booking.objects.filter(email='jane@test.com')
-        booking = get_object_or_404(booking_query)
+    # def post(self, request, *args, **kw):
+    #     booking_query = Booking.objects.filter(email='jane@test.com')
+    #     booking = get_object_or_404(booking_query)
 
-        booking_form = BookingForm(data=request.POST)
-        if booking_form.is_valid():
-            booking_form.save(commit=False)
-            if booking_form.cleaned_data['num_of_people'] == 4:
-                table_query = Table.objects.filter(table_num=1)
-                table = get_object_or_404(table_query)
-                booking_form.instance.table_num = table
-            else:
-                table_query1 = Table.objects.filter(table_num=2)
-                table1 = get_object_or_404(table_query1)
-                booking_form.instance.table_num = table1
-            booking_form.save()
+    #     booking_form = BookingForm(data=request.POST)
+    #     if booking_form.is_valid():
+    #         booking_form.save(commit=False)
+    #         if booking_form.cleaned_data['num_of_people'] == 4:
+    #             table_query = Table.objects.filter(table_num=1)
+    #             table = get_object_or_404(table_query)
+    #             booking_form.instance.table_num = table
+    #         else:
+    #             table_query1 = Table.objects.filter(table_num=2)
+    #             table1 = get_object_or_404(table_query1)
+    #             booking_form.instance.table_num = table1
+    #         booking_form.save()
 
-            return render(request, 'booking.html', {
-                "booking": booking,
-                "booking_form": BookingForm()
-            })
-        else:
-            return render(request, "index.html")
+    #         return render(request, 'booking.html', {
+    #             "booking": booking,
+    #             "booking_form": BookingForm()
+    #         })
+    #     else:
+    #         return render(request, "index.html")
